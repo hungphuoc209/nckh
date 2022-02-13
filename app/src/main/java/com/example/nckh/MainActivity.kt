c97c1e288ce5df9970046938a8b09e02da5240d9
hungphuoc209 <nguyenhungphuoc209@gmail.com>
2022-02-13 21:21:15 +0700

    initial commit

---
+++ app/src/main/java/com/example/nckh/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.nckh
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.sms.ezviewbinding.viewBinding
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nckh.databinding.ActivityMainBinding
+import com.google.mlkit.common.model.LocalModel
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.custom.CustomImageLabelerOptions
+import com.theartofdev.edmodo.cropper.CropImage
+
+
+class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+    private val localModel = LocalModel.Builder().setAssetFilePath("model.tflite").build()
+    private val labels = arrayListOf("Healthy","Late Blight")
+
+    private val customImageLabelerOptions = CustomImageLabelerOptions.Builder(localModel)
+        .setConfidenceThreshold(0.5f)
+        .setMaxResultCount(5)
+        .build()
+    private val labeler = ImageLabeling.getClient(customImageLabelerOptions)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        initListeners()
+    }
+
+
+    private val cropActivityResultContract = object : ActivityResultContract<Any?, Uri?>(){
+        override fun createIntent(context: Context, input: Any?): Intent {
+            return CropImage.activity()
+                .setAspectRatio(1,1)
+                .getIntent(this@MainActivity)
+        }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+            return CropImage.getActivityResult(intent)?.uri
+        }
+    }
+
+    private lateinit var cropActivityLauncher: ActivityResultLauncher<Any?>
+
+    private fun initListeners() {
+        cropActivityLauncher = registerForActivityResult(cropActivityResultContract){uri ->
+            uri?.let {
+                var bitmapImage : Bitmap? = null
+                bitmapImage = if(Build.VERSION.SDK_INT < 28) {
+                    val bitmap = MediaStore.Images.Media.getBitmap(
+                        this.contentResolver,
+                        uri)
+                    bitmap
+                } else {
+                    val source = ImageDecoder.createSource(this.contentResolver, uri)
+                    val bitmap = ImageDecoder.decodeBitmap(source)
+                    bitmap
+                }
+                if(bitmapImage!=null) {
+                    val image = InputImage.fromBitmap(bitmapImage, 0)
+                    labeler.process(image)
+                        .addOnSuccessListener {
+                            binding.tvResult.text = labels[it[0].index].toString()
+                        }
+                        .addOnFailureListener {
+                            binding.tvResult.text = it.localizedMessage
+                        }
+                    binding.imgPicture.setImageBitmap(bitmapImage)
+                }
+            }
+        }
+
+        binding.btnAdd.setOnClickListener{
+            cropActivityLauncher.launch(null)
+        }
+    }
+}
